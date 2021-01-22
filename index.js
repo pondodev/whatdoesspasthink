d16cfb08409792f31f1dbfbfe692dfe72e47dd18
Daniel Coady <me@pondo.dev>
2021-01-23 09:57:52 +1100

    remove console log from js

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ window.onload = () => {
     var index = Math.floor(Math.random() * Math.floor(maxReviews));
     var id = "review-" + index;
 
-    console.log(id);
     document.getElementById(id).style = "display: block"
 }
